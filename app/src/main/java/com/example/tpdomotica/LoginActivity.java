3b1b5d943a465e114d0c10abfedd9c3f638398e5
ivanzapata97 <ivanzapata.f@gmail.com>
2019-06-21 19:52:33 -0300

    cerrar sesion, borrar y leer sharedpreferences

--- app/src/main/java/com/example/tpdomotica/LoginActivity.java
+++ app/src/main/java/com/example/tpdomotica/LoginActivity.java
@@ -9,15 +9,18 @@ import android.text.TextUtils;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class LoginActivity extends AppCompatActivity {
 
     private SharedPreferences prefs;
 
-    private EditText editTextEmail;
+    private EditText editTextEmail; //estos dos atributos tienen  que ser EditText para que el usuario pueda ingresar los datos
     private EditText editTextPassword;
     private Switch switchRecordar;
     private Button btnLogin;
@@ -27,10 +30,12 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login3);
-
+        bindUI();
         prefs = getSharedPreferences("Preferences", Context.MODE_PRIVATE);
 
-        bindUI();
+        setCredentialsIfExist();
+
+
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -46,11 +51,21 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void bindUI(){
-        editTextEmail = (EditText)findViewById(R.id.editTextEmail);
+        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
         editTextPassword = (EditText) findViewById(R.id.editTextPassword);
         switchRecordar = (Switch) findViewById(R.id.switchRecordar);
         btnLogin = (Button) findViewById(R.id.btnLogin);
     }
+
+    public void setCredentialsIfExist(){
+        String email = getUserMailPrefs();
+        String password = getUserPasswordPrefs();
+        if(!TextUtils.isEmpty(email) && !TextUtils.isEmpty((password))){
+            editTextEmail.setText(email);
+            editTextPassword.setText(password);
+        }
+    }
+
     private boolean login(String email, String password){
         if (!isValidEmail(email)){
             Toast.makeText(this, "email no valido, intenta de nuevo", Toast.LENGTH_LONG).show();
@@ -83,6 +98,13 @@ public class LoginActivity extends AppCompatActivity {
         Intent intent = new Intent(this, MainActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(intent);
+    }
+
+    private String getUserMailPrefs(){
+        return prefs.getString("email","");
+    }
+    private String getUserPasswordPrefs(){
+        return prefs.getString("pass","");
     }
 
 
--- app/src/main/java/com/example/tpdomotica/MainActivity.java
+++ app/src/main/java/com/example/tpdomotica/MainActivity.java
@@ -1,13 +1,51 @@
 package com.example.tpdomotica;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SharedPreferences prefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        prefs = getSharedPreferences("preferences", Context.MODE_PRIVATE);
+    }
+
+    @Override
+    public boolean onCreateOptionMnu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    @Override
+    public boolean onOptionItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.menu_logout:
+                logOut();
+                return true;
+            case R.id.menu_forget_logout:
+                removeSharedPreferences();
+                logOut();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+    private void logOut(){
+        Intent intent = new Intent(this, LoginActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+    }
+    private void removeSharedPreferences(){
+        prefs.edit().clear().apply();
+
     }
 }
