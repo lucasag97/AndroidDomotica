cedcb9cda2126a19e1216df982ee6c9f45fcc8b7
ivanzapata97 <ivanzapata.f@gmail.com>
2019-06-28 23:08:44 -0300

    toolbar, cerrar sesion

--- app/src/main/java/com/example/tpdomotica/MainActivity.java
+++ app/src/main/java/com/example/tpdomotica/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.tpdomotica;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -38,5 +41,30 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(login);
             }
         });
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu,menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.menu_edificio:
+                Intent intent = new Intent(getApplicationContext(),EdificioActivity.class);
+                startActivity(intent);
+                return true;
+            case R.id.menu_logout:
+                Intent cerrar_sesion = new Intent(getApplicationContext(),LoginActivity.class);
+                startActivity(cerrar_sesion);
+                return true;
+            case R.id.menu_forget_logout:
+                Intent cerrar_sesion_olvidar = new Intent(getApplicationContext(),LoginActivity.class);
+                startActivity(cerrar_sesion_olvidar);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
